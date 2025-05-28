7ce8d9784e9b6d3c1f9af258c51966678b66778e
Vasili2797 <nashvilliv1@gmail.com>
2025-05-28 19:08:33 -0400

    added the express-async-error library

--- Part4/Step4.6/controllers/notes.js
+++ Part4/Step4.6/controllers/notes.js
@@ -6,30 +6,16 @@ notesRouter.get("/", async (request, response) => {
   response.json(notes);
 });
 
-notesRouter.get("/:id", async (request, response, next) => {
-  try {
-    const note = await Note.findById(request.params.id);
-    if (note) {
-      response.json(note);
-    } else {
-      response.status(404).end();
-    }
-  } catch (exception) {
-    next(exception);
+notesRouter.get("/:id", async (request, response) => {
+  const note = await Note.findById(request.params.id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
   }
-
-  // Note.findById(request.params.id)
-  //   .then((note) => {
-  //     if (note) {
-  //       response.json(note);
-  //     } else {
-  //       response.status(404).end();
-  //     }
-  //   })
-  //   .catch((error) => next(error));
 });
 
-notesRouter.post("/", async (request, response, next) => {
+notesRouter.post("/", async (request, response) => {
   const body = request.body;
 
   const note = new Note({
@@ -37,32 +23,13 @@ notesRouter.post("/", async (request, response, next) => {
     important: body.important || false,
   });
 
-  // note
-  //   .save()
-  //   .then((savedNote) => {
-  //     response.status(201).json(savedNote);
-  //   })
-  //   .catch((error) => next(error));
-  try {
-    const savedNote = await note.save();
-    response.status(201).json(savedNote);
-  } catch (exception) {
-    next(exception);
-  }
+  const savedNote = await note.save();
+  response.status(201).json(savedNote);
 });
 
-notesRouter.delete("/:id", async (request, response, next) => {
-  try {
-    await Note.findByIdAndDelete(request.params.id);
-    response.status(204).end();
-  } catch (exception) {
-    next(exception);
-  }
-  // Note.findByIdAndDelete(request.params.id)
-  //   .then(() => {
-  //     response.status(204).end();
-  //   })
-  //   .catch((error) => next(error));
+notesRouter.delete("/:id", async (request, response) => {
+  await Note.findByIdAndDelete(request.params.id);
+  response.status(204).end();
 });
 
 notesRouter.put("/:id", (request, response, next) => {
